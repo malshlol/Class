49dab0bf1c53a3235e80d17741a54bbc8b89ad4c
lolze <lolzek@bk.ru>
2023-02-19 19:43:40 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
 public class Main {
+
+    public static void main(String[] args) {
+    Author gogol = new Author("Николай", " Гоголь");
+    Author dostoevskiy = new Author("Фёдор", " Достоевский");
+
+
+
+    Book viy = new Book("Вий", 1833, gogol);
+    Book prestyplenieINakazanie = new Book("Преступлений и наказание", 1865, dostoevskiy);
+
+
+    viy.printInfo();
+    prestyplenieINakazanie.printInfo();
+
+viy.setYear(1866);
+        viy.printInfo();
+    }
 }
